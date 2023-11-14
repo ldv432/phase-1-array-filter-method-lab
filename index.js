e1c52677c4b6a40999add1e1842b2a206c764dcd
Luke <ldv432@gmail.com>
2023-11-14 00:12:42 -0700

    some filters

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, name){
+    const lowercaseDrivers = name.toLowerCase()
+    const matchingDrivers = drivers.filter((driver) => {
+        const lowercaseDriver = driver.toLowerCase();
+        return lowercaseDriver.startsWith(lowercaseDrivers)
+    });
+    return matchingDrivers;
+}
+
+function fuzzyMatch(names, passInName){
+    const sameStart = names.filter((driver) => {
+    return driver.startsWith(passInName);})
+    return sameStart;
+}
+
+function matchName(drivers, string){
+    return drivers.filter((driver) => driver.name === string)
+}
